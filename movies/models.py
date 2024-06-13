3feafb3bf023536b6d704d08d5699ea3588ec6a8
Finley <finley.mwachia12@gmail.com>
2024-06-13 13:38:31 +0300

    add new class

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.charField(max_length = 255)
+
+class Movie(models.model):
+    name = models.CharField(max_length = 200)
+    
